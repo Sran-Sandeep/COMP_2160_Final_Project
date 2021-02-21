894e288f3505c0b253b4636f87699ebdc6dad508
Sran-Sandeep <77460117+Sran-Sandeep@users.noreply.github.com>
2021-02-21 11:50:37 -0800

    Basic list for main page

--- app/src/main/java/com/example/comp_2160_final_project/MainActivity.java
+++ app/src/main/java/com/example/comp_2160_final_project/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.comp_2160_final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    String main_list[] = new String[] {"Option1", "Option2", "Option3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       ListView listView = (ListView) findViewById(R.id.main_listview);
+       ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,main_list);
+       listView.setAdapter(adapter);
+
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // Link to the next Xml files
+           }
+       });
     }
+
+
 }
