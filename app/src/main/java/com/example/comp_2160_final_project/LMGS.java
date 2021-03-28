766f8e612db6e363668046dc217be2200782ef8f
Dwaine Algate <juniper1915@outlook.com>
2021-03-28 14:22:23 -0700

    lmg activity

--- app/src/main/java/com/example/comp_2160_final_project/LMGS.java
+++ app/src/main/java/com/example/comp_2160_final_project/LMGS.java
@@ -1,14 +1,30 @@
 package com.example.comp_2160_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class LMGS extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String[] s1;
+    String[] s2;
+    int[] images = {R.drawable.icon_pkm,R.drawable.icon_sa87,R.drawable.icon_m91, R.drawable.icon_mg34};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_l_m_g_s);
+
+        recyclerView = findViewById(R.id.lmgRecycler);
+
+        s1 = getResources().getStringArray(R.array.rLMG);
+        //s2 = add descriptions or whatever
+
+        newAdapter myAdapter = new newAdapter(this,s1,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
