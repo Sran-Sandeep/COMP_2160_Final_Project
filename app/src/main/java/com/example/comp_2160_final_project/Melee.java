e408c74cbdb0acbb966166ac3042ab61d4669185
Dwaine Algate <juniper1915@outlook.com>
2021-03-29 13:58:33 -0700

    melee activity

--- app/src/main/java/com/example/comp_2160_final_project/Melee.java
+++ app/src/main/java/com/example/comp_2160_final_project/Melee.java
@@ -1,14 +1,31 @@
 package com.example.comp_2160_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class Melee extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String[] s1;
+    String[] s2;
+    int[] images = {R.drawable.icon_combatknife,R.drawable.icon_riot_shield};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_melee);
+
+        recyclerView = findViewById(R.id.meleeRecycler);
+
+        s1 = getResources().getStringArray(R.array.rMelee);
+        //s2 = add descriptions or whatever
+
+        newAdapter myAdapter = new newAdapter(this,s1,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
