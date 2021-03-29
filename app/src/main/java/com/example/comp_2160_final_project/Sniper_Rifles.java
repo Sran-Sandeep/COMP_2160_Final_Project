f6cb9c4f78748c4baeab89f716416925980440eb
Dwaine Algate <juniper1915@outlook.com>
2021-03-29 13:43:18 -0700

    sniper activity

--- app/src/main/java/com/example/comp_2160_final_project/Sniper_Rifles.java
+++ app/src/main/java/com/example/comp_2160_final_project/Sniper_Rifles.java
@@ -1,14 +1,30 @@
 package com.example.comp_2160_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class Sniper_Rifles extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String[] s1;
+    String[] s2;
+    int[] images = {R.drawable.icon_dragunov,R.drawable.icon_hdr,R.drawable.icon_ax_50};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sniper__rifles);
+
+        recyclerView = findViewById(R.id.SniperRecycler);
+
+        s1 = getResources().getStringArray(R.array.rSR);
+        //s2 = add descriptions or whatever
+
+        newAdapter myAdapter = new newAdapter(this,s1,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-}
+    }
