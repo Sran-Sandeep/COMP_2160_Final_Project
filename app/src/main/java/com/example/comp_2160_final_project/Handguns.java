568d03ede9017d3847633fbbd37d0228797034d5
Dwaine Algate <juniper1915@outlook.com>
2021-03-29 13:54:58 -0700

    handgun activity

--- app/src/main/java/com/example/comp_2160_final_project/Handguns.java
+++ app/src/main/java/com/example/comp_2160_final_project/Handguns.java
@@ -1,66 +1,31 @@
 package com.example.comp_2160_final_project;
 
-import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
 
 public class Handguns extends AppCompatActivity {
-int picker = 0;
-//String s1[], s2[];
-//int images[] = {R.drawable.icon_1911,R.drawable.icon_x16,R.drawable.icon_357,R.drawable.icon_m19, R.drawable.icon_50gs, R.drawable.icon_renetti};
-ImageButton but1, but2, but3, but4, but5, but6, but7, but8, but9, but10;
-TextView TextTitle, Textchallenge;
-ImageView gunView;
+
+    RecyclerView recyclerView;
+
+    String[] s1;
+    String[] s2;
+    int[] images = {R.drawable.icon_x16,R.drawable.icon_1911,R.drawable.icon_357, R.drawable.icon_m19, R.drawable.icon_50gs};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_1911);
-        but1 = findViewById(R.id.imageBtn1);
-        but2 = findViewById(R.id.imageBtn2);
-        but3 = findViewById(R.id.imageBtn3);
-        but4 = findViewById(R.id.imageBtn4);
-        but5 = findViewById(R.id.imageBtn5);
-        but6 = findViewById(R.id.imageBtn6);
-        but7 = findViewById(R.id.imageBtn7);
-        but8 = findViewById(R.id.imageBtn8);
-        but9 = findViewById(R.id.imageBtn9);
-        but10 = findViewById(R.id.imageBtn10);
-        TextTitle = findViewById(R.id.titleText);
-        gunView = findViewById(R.id.imageView_weapon);
+        setContentView(R.layout.activity_handguns);
 
-        //s1 = getResources().getStringArray(R.array.handguns);
+        recyclerView = findViewById(R.id.handgunRecycler);
 
-        switch (picker)
-        {
-            case 0:
-                enact_1911();
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-        }
+        s1 = getResources().getStringArray(R.array.rHandgun);
+        //s2 = add descriptions or whatever
 
-    }
-
-    public void enact_1911()
-    {
-        //TextTitle.setText("1911");
-        gunView.setImageResource(R.drawable.icon_1911);
-        but1.setImageResource(R.drawable.camo_digital_2);
-        but2.setImageResource(R.drawable.camo_digital_1);
-        but3.setImageResource(R.drawable.camo_digital_3);
-        but4.setImageResource(R.drawable.camo_digital_4);
-        but5.setImageResource(R.drawable.camo_digital_5);
-        but6.setImageResource(R.drawable.camo_digital_6);
-        but7.setImageResource(R.drawable.camo_digital_7);
-        but8.setImageResource(R.drawable.camo_digital_8);
-        but9.setImageResource(R.drawable.camo_digital_9);
-        but10.setImageResource(R.drawable.camo_completionist_damascus);
-
+        newAdapter myAdapter = new newAdapter(this,s1,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
