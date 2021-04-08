2fe10ff8dbe68b853a34975c57576a465e2769a4
ianfuentes412 <ianfuentes412@users.noreply.github.com>
2021-04-08 12:36:45 -0700

    Implemented SharedPreferences

--- app/src/main/java/com/example/comp_2160_final_project/Launchers.java
+++ app/src/main/java/com/example/comp_2160_final_project/Launchers.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class Launchers extends AppCompatActivity {
 
     RecyclerView recyclerView;
-
+    SharedPreferences preferences;
     String[] s1;
     String[] s2;
     int[] images = {R.drawable.icon_pila,R.drawable.icon_strela_p,R.drawable.icon_jokr, R.drawable.icon_rpg_7};
@@ -17,14 +22,43 @@ public class Launchers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launchers);
-
+        //CheckBox checkbox = (CheckBox)findViewById(R.id.checkDone);
         recyclerView = findViewById(R.id.launcherRecycler);
+        //preferences = getSharedPreferences(MYPREFERENCES, Context.MODE_PRIVATE);
+        //.Editor editor = preferences.edit();
+
+
 
         s1 = getResources().getStringArray(R.array.rlaunchers);
-        //s2 = add descriptions or whatever
+
 
         newAdapter myAdapter = new newAdapter(this,s1,images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        /*checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkbox.isChecked())
+                {
+                    editor.putBoolean("checked",true);
+                }
+                else
+                {
+                    editor.putBoolean("checked",false);
+                }
+                editor.apply();
+            }
+        }); */
+
+               /* if(preferences.contains("checked") && preferences.getBoolean("checked",false) == true) {
+            checkbox.setChecked(true);
+        }
+        else{
+            checkbox.setChecked(false);
+        }
+*/
     }
+
+
 }
--- app/src/main/java/com/example/comp_2160_final_project/newAdapter.java
+++ app/src/main/java/com/example/comp_2160_final_project/newAdapter.java
@@ -1,27 +1,35 @@
 package com.example.comp_2160_final_project;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class newAdapter extends RecyclerView.Adapter<newAdapter.MyViewHolder> {
 
+    public static final String MYPREFERENCES = "MyPrefs";
     String[] data;
     int[] images;
     Context context;
-
+    SharedPreferences preferences;
 
     public newAdapter(Context ct, String[] s1, int[] img)
     {
         context = ct;
         data = s1;
         images = img;
+        this.preferences = ct.getSharedPreferences(MYPREFERENCES,Context.MODE_PRIVATE);
     }
 
     @NonNull
@@ -36,6 +44,18 @@ public class newAdapter extends RecyclerView.Adapter<newAdapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.TextL.setText(data[position]);
         holder.ImageL.setImageResource(images[position]);
+        String key = holder.TextL.getText().toString();
+        boolean hasItem = preferences.getBoolean(key,false);
+        holder.checkBox.setChecked(hasItem);
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.v("tomato",key +" "+ isChecked);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean(key,isChecked);
+                editor.commit();
+            }
+        });
     }
 
     @Override
@@ -44,13 +64,15 @@ public class newAdapter extends RecyclerView.Adapter<newAdapter.MyViewHolder> {
     }
 
     public class MyViewHolder extends  RecyclerView.ViewHolder {
-
+        CheckBox checkBox;
         TextView TextL;
         ImageView ImageL;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             TextL = itemView.findViewById(R.id.weaponTitle);
             ImageL = itemView.findViewById(R.id.weaponImage);
+            checkBox = itemView.findViewById(R.id.checkDone);
+
         }
     }
 }
